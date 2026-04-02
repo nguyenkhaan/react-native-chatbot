d45a840a8296a7f1c470bc95a9da24f78d8dacc3
nguyenkhaan <nguyenkhaan2006@gmail.com>
2026-04-02 19:16:13 +0700

    chore: initial folder strucure

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
+import { Text } from 'react-native';
 import './global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <SafeAreaProvider>
+      <Text>Hello world</Text>
+      <Text className="text-red-500">Hello everybody. This is the first Kamen Rider project</Text>
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
     </SafeAreaProvider>
---
+++ note.md
@@ -0,0 +1,21 @@
+# React Native
+
+## Setup the project
+
+- Using npm
+
+```
+npx rn-new --nativewind
+```
+
+- Using bun
+
+```
+bunx rn-new --nativewind
+```
+
+-After setup complete. Delete the node_modules folder and run `bun install` again for synchronization
+
+## Setup husky and commitlint lint-staged
+
+- https://dev.to/mdasif-me/step-by-step-guide-to-setting-up-husky-eslint-and-prettier-in-a-nextjs-typescript-project-2f2l
